a5a98d5c19890850f52c77947c61ad043508f95a
Aman Ibrahim <amanmibra@gmail.com>
2023-09-19 11:21:07 -0700

    Introduce CapClient

--- cap.py
+++ cap.py
@@ -2,9 +2,16 @@ import tweepy
 
 from config import cap_token
 
-print("cap token:", cap_token)
-client = tweepy.Client(cap_token)
-print("client", client)
+class CapClient:
+    def __init__(self) -> None:
+        self.client = tweepy.Client(cap_token)
+        self.me = self.client.get_me(user_auth=False).data
 
-client.create_tweet(text="I AM ALIVE", user_auth=False)
-# print(client.get_me(user_auth=False))
+    def tweet(self, text, **args):
+        return self.client.create_tweet(text=text, user_auth=False, **args)
+    
+    def get_mentions(self, **args):
+        return self.client.get_users_mentions(id=self.me.id, **args)
+
+def get_client():
+    return tweepy.Client(cap_token)
