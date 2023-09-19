0fd3b54e8cd07ef6d18bb60b96607208482c86f5
Aman Ibrahim <amanmibra@gmail.com>
2023-09-19 15:28:20 -0700

    Update client

--- cap.py
+++ cap.py
@@ -1,17 +1,25 @@
 import tweepy
 
-from config import cap_token
+import config
 
 class CapClient:
     def __init__(self) -> None:
-        self.client = tweepy.Client(cap_token)
-        self.me = self.client.get_me(user_auth=False).data
+        self.client = get_client()
+        self.me = self.client.get_me().data
 
     def tweet(self, text, **args):
-        return self.client.create_tweet(text=text, user_auth=False, **args)
+        return self.client.create_tweet(text=text, **args)
     
     def get_mentions(self, **args):
         return self.client.get_users_mentions(id=self.me.id, **args)
 
-def get_client():
-    return tweepy.Client(cap_token)
+def get_client() -> tweepy.API:
+    return tweepy.Client(config.bearer_token,  consumer_key=config.api_key, consumer_secret=config.api_secret, access_token=config.access_token, access_token_secret=config.access_secret)
+
+def get_api():
+    auth = tweepy.OAuthHandler(config.api_key, config.api_key)
+    auth.set_access_token(config.access_token, config.access_secret)
+
+    api = tweepy.API(auth)
+    
+    return api
