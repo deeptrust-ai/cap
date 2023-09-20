0b80f57a6e21b246c4fbcb361b2061f9d314292d
Aman Ibrahim <amanmibra@gmail.com>
2023-09-20 16:56:25 -0700

    Add modal function to launch_job

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ from datetime import datetime
 import logging
 import time
 
+from modal import Function
+
 from cap import CapClient, ValidMention
 
 FORMAT = '%(asctime)s: %(message)s'
@@ -12,17 +14,28 @@ logging.info("Capper Checker starting...")
 
 def launch_job(mention: ValidMention) -> None:
     # will launch a modal job to fact check tweet (API request)
-    # will launch a poller job to update with tweet (modal function launch)
-    pass
+    # TODO: Deploy changes to modal
+    logging.info(f"Launching twitter predict job for mention(id={mention.mention.id})...")
+    twitter_predict = Function.lookup("rawnet-predict-jobs", "twitter_predict")
+    parent_tweet = mention.parent_tweet.data
+    predict_job = twitter_predict.spawn(parent_tweet.id, "ss")
+    job_id = predict_job.object_id
+
+    # launch a poller job to update with tweet (modal function launch)
+    logging.info("Launching poller...")
+    poller = Function.lookup("cap-poller", "poller")
+    poller.spawn(job_id)
+
 
 start_time = datetime.now()
 while True:
     # Get the mentions for your bot's user ID.
     logging.info(f"Getting mentions starting from time ({start_time})")
+    # TODO: Fix start_time
     mentions = cap.get_mentions(start_time=start_time)
     
-    for mention in mentions:
-        launch_job(mention)
+    # for mention in mentions:
+        # launch_job(mention)
     
     # update start time
     start_time = datetime.now()
