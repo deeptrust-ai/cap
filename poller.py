cf267ec11825dd824dff3db46429d88e480c21eb
Aman Ibrahim <amanmibra@gmail.com>
2023-09-25 00:09:26 -0700

    Add heatmap to poller

--- poller.py
+++ poller.py
@@ -3,6 +3,7 @@ import logging
 from modal import functions, Image, Stub, Secret
 
 from cap import CapClient
+from heatmap import create_heatmap, delete_heatmap
 
 FORMAT = '%(asctime)s: %(message)s'
 logging.basicConfig(format=FORMAT, level=logging.INFO)
@@ -19,7 +20,7 @@ stub = Stub(
     "cap-poller",
     image=image,
     secrets=(
-        Secret.from_dotenv(),
+        Secret.from_dotenv(".env.prod"),
     ),
 )
 
@@ -40,11 +41,20 @@ async def poller(id: str, mention_tweet_id: int):
     # TODO: Add tweet
     logging.info(f"Job(id={id}, mention_tweet_id={mention_tweet_id}) completed with this result: {result}")
     scores = result.get("scores")
+    segmented_predictions = result.get("segmented_predictions")
     if not scores:
         raise ScorelessException(f"Job(id={id}, mention_tweet_id={mention_tweet_id}) returned empty scores.")
 
-    logging.info(f"Job(id={id}, mention_tweet_id={mention_tweet_id}) posting to twitter...")    
-    cap.tweet(text=_tweet(scores[0]), in_reply_to_tweet_id=mention_tweet_id)
+    logging.info(f"Job(id={id}, mention_tweet_id={mention_tweet_id}) posting to twitter...") 
+    media_ids = []
+    for sg in segmented_predictions:
+        create_heatmap(sg)
+        media = cap.api.media_upload("heatmap.png")
+        media_ids.append(media.media_id)
+        delete_heatmap()
+
+    
+    cap.tweet(text=_tweet(scores[0]), in_reply_to_tweet_id=mention_tweet_id, media_ids=media_ids)
 
 
 def _tweet(score: int):
