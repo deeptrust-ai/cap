bf13bdb308fee0cf5aa313cd89e3a847f75e8b24
Aman Ibrahim <amanmibra@gmail.com>
2023-10-12 18:59:30 -0700

    Gen heatmap script

---
+++ scripts/gen_heatmap.py
@@ -0,0 +1,4 @@
+from heatmap import create_heatmap, delete_heatmap
+data = [0.9, 1.0, 1.0, 1.0, 0.1, 0.1, 0.1, 0.1, 0.0, 0.9, 0.1, 0.9, 0.9, 0.9, 1.0, 0.1, 0.0, 0.1, 1.0, 0.9, 0.9, 1.0, 0.1]
+
+create_heatmap(data)
